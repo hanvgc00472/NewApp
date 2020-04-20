3071960f9ec736c901ec2efea9658e289d31b688
Nguyễn Việt Hà <hanvgc00472@gmail.com>
2020-04-20 22:33:19 +0700

    upp

--- NewDevelopmentApp/AppDelegate.swift
+++ NewDevelopmentApp/AppDelegate.swift
@@ -19,10 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MSDistributeDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch
-        MSDistribute.setDelegate(self);
+//        MSDistribute.setDelegate(self);
         MSDistribute.updateTrack = MSUpdateTrack.public
-        MSDistribute.setEnabled(true)
-        MSDistribute.checkForUpdate()
+//        MSDistribute.setEnabled(true)
+//        MSDistribute.checkForUpdate()
         MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices:[
           MSAnalytics.self,
           MSCrashes.self,
