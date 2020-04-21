abfd1db609181f17afcc94c06d2913b813611f2f
Nguyễn Việt Hà <hanvgc00472@gmail.com>
2020-04-21 17:04:00 +0700

    xyzzzzz

--- NewDevelopmentApp/AppDelegate.swift
+++ NewDevelopmentApp/AppDelegate.swift
@@ -19,25 +19,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MSDistributeDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch
-        MSDistribute.setDelegate(self);
-        MSDistribute.checkForUpdate()
+     
+        MSDistribute.setDelegate(self)
+           MSDistribute.checkForUpdate()
        
         MSDistribute.updateTrack = MSUpdateTrack.public
 
-        MSDistribute.checkForUpdate()
+//        MSDistribute.checkForUpdate()
         MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices:[
           MSAnalytics.self,
           MSCrashes.self,
           MSDistribute.self
         ])
         
-                var enabled = MSDistribute.isEnabled()
-
-        #if DEBUG
-           MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices: [MSAnalytics.self, MSCrashes.self])
-        #else
-           MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices: [MSAnalytics.self, MSCrashes.self, MSDistribute.self])
-        #endif
+//                var enabled = MSDistribute.isEnabled()
+//
+//        #if DEBUG
+//           MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices: [MSAnalytics.self, MSCrashes.self])
+//        #else
+//           MSAppCenter.start("1cb21983-1bbe-4d23-93d5-4e528e0f1c49", withServices: [MSAnalytics.self, MSCrashes.self, MSDistribute.self])
+//        #endif
         return true
     }
     func distribute(_ distribute: MSDistribute!, releaseAvailableWith details: MSReleaseDetails!) -> Bool {
